247830ca1a62f67b8b8a88c706d4e1b8ba947820
minkyeong <dufma9385@gmail.com>
2020-02-01 11:55:59 +0900

    client checkbox 수정

--- public/js/client.js
+++ public/js/client.js
@@ -1,19 +1,19 @@
 $(document).ready(function(){
-    $('#login_bt1').click(function(){
-        //alert();
-        const contact_email=$('#login_email').val();
-        const send_param={contact_email};
-        $.post('contact', send_param,function(returnData){
+    $('#sign_btn').click(function(){
+        const user_email = $('#user_email').val();
+        const user_name = $('#user_email').val();
+        const user_phone = $('#user_phone').val();
+        const send_param = {user_email, user_name, user_phone};
+        $.post('contact', send_param, function(returnData){
             alert(returnData.message);
         });
     });
-
     $('#sign').click(function(){
         let sign_html_content="<div class='col-lg-6 col-md-6 contact-left-form'>";
         sign_html_content +="<form>";
         sign_html_content +="<div class='form-group contact-forms'>";
         sign_html_content +="<label for='email'>Email:</label>";
-        sign_html_content +="<input type='email' class='form-control' id='email' placeholder='Enter email'>";
+        sign_html_content +="<input type='email' class='form-control' id='user_email' placeholder='Enter email'>";
         sign_html_content +="</div>";
         sign_html_content +="<div class='form-group'>";
         sign_html_content +="<label for='name'>Name:</label>";
@@ -21,15 +21,20 @@ $(document).ready(function(){
         sign_html_content +="</div>";
         sign_html_content +="<div class='form-group'>";
         sign_html_content +="<label for='phone'>Phone:</label>";
-        sign_html_content +="<input type='' class='form-control' id='user_name' placeholder='Enter your name'>";
-        sign_html_content +="</div>";
-        sign_html_content +="<div class='checkbox'>";
-        sign_html_content +="<label><input type='checkbox'> Remember me</label>";
-        sign_html_content +="</div>";
-        sign_html_content +="<input type='button' id='login_b' class='btn btn-block sent-butnn' value='Send'></form>";
+        sign_html_content +="<input type='' class='form-control' id='user_phone' placeholder='Enter your name'>";
+        sign_html_content +="<input type='button' id='sign_btn' class='btn btn-block sent-butnn' value='Send'></form>";
         sign_html_content +="</div>"
         $('#footer').html(sign_html_content);
 
+    });
+
+    $('#login_bt1').click(function(){
+        //alert();
+        const contact_email=$('#login_email').val();
+        const send_param={contact_email};
+        $.post('contact', send_param,function(returnData){
+            alert(returnData.message);
+        });
     });
     
     $('#login_btn').click(function(){
